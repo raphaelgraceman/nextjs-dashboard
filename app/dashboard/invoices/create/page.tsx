6ac362f22be4a214acaea0d6e9191acbb904b227
Dotse Raphael <raphaelgraceman@gmail.com>
2025-06-06 09:53:17 +0000

    Added Metadata

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,14 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
- 
+ import { Metadata } from 'next';
+
+//Adding Metadata
+export const metadata: Metadata = {
+  title: 'Create Invoice',
+}
+
+
 export default async function Page() {
   const customers = await fetchCustomers();
  
