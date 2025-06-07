e568e2a3eaf60a12fcce7f1f8e2e3e444cd889b1
Dotse Raphael <raphaelgraceman@gmail.com>
2025-06-07 21:35:57 +0000

    Updated customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,25 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 
-//Adding Metadata
 export const metadata: Metadata = {
   title: 'Customers',
-}
+};
 
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
 
+  const customers = await fetchFilteredCustomers(query);
 
-export default function Page() {
-  return <p>Customers Page</p>;
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }
